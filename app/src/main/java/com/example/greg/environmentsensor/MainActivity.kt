a7dcfb4769e85c7794c94d62a078460cdd03fcdc
Greg Dick <gdick@mercuryintermedia.com>
2020-06-12 12:30:40 -0500

    Added barometric pressure sensor

--- app/src/main/java/com/example/greg/environmentsensor/MainActivity.kt
+++ app/src/main/java/com/example/greg/environmentsensor/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.greg.environmentsensor
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+
+    private lateinit var sensorManager: SensorManager
+    private var pressure: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+    }
+
+    override fun onResume() {
+        // Register a listener for the sensor.
+        super.onResume()
+        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val millibarsOfPressure = event?.values?.get(0) ?: 0f
+        val inchesMercury = millibarsOfPressure * CONVERT_MBAR_TO_INHG
+        val df = DecimalFormat("0.00")
+        pressure_value.text = getString(R.string.pressure_value, df.format(inchesMercury))
+    }
+
+    companion object {
+        const val CONVERT_MBAR_TO_INHG = 0.02953
     }
 }
